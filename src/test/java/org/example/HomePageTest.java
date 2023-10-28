e68357c1e3877fd284f5141d5ea9c02f764b9399
Pravin Ekawade <pravin@Pravins-MacBook-Air.local>
2023-10-28 20:55:04 +0530

    Test commit

---
+++ src/test/java/org/example/HomePageTest.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class HomePageTest {
+
+    public static void main(String[] args){
+        System.out.println("HomePageTest");
+    }
+}
---
+++ src/test/java/org/example/LoginPageTest.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class LoginPageTest {
+
+    public void test(){
+        System.out.println("test 1 - LP");
+    }
+}
