6c6b4a378256eb386aa5abf46c8a119994551f05
Pravin-E <ppe422@gmail.com>
2023-10-28 21:48:32 +0530

    Modified Page and Test cases

--- src/main/java/org/example/SearchPage.java
+++ src/main/java/org/example/SearchPage.java
@@ -3,5 +3,6 @@ package org.example;
 public class SearchPage {
     public void search(){
         System.out.println("Search");
+        System.out.println("Product searched");
     }
 }
--- src/test/java/org/example/LoginPageTest.java
+++ src/test/java/org/example/LoginPageTest.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 public class LoginPageTest {
 
     public void test(){
         System.out.println("test 1 - LP");
+        System.out.println("test2 - LP");
     }
 }
